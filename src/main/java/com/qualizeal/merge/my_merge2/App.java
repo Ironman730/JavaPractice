83e24dec6604681c54dd1ed9c54eea2599aacdc6
IronMan@007 <adilsamdani3003@gmail.com>
2023-06-20 01:15:08 +0530

    Merge commit

---
+++ src/main/java/com/qualizeal/merge/my_merge2/App.java
@@ -0,0 +1,24 @@
+package com.qualizeal.merge.my_merge2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        // This merge maven app is to show you how can we merge 2 different projects using git
+        System.out.println("Some Other operations are been performed");
+    	int a1 = 12;
+    	int b1 = 13;
+    	
+    	if(a1>b1) {
+    		System.out.println("a1 is greater than b1");
+    	}else {
+    		System.out.println("b1 is greater than a1");
+    	}
+    }
+}
