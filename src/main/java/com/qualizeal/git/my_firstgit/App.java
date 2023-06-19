fae1d137b1b4e75d3cc905bcaca6f1cf49a9e81d
venkat <venkatkumardondapati999@gmail.com>
2023-06-20 00:35:01 +0530

    Venkat commit

--- src/main/java/com/qualizeal/git/my_firstgit/App.java
+++ src/main/java/com/qualizeal/git/my_firstgit/App.java
@@ -1,8 +1,11 @@
 package com.qualizeal.git.my_firstgit;
+import com.qualizeal.git.my_firstgit.venkatgit.Oerations;
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+    	
+    	Oerations operations = new Oerations();
+    	
         System.out.println( "Hello World!" );
         System.out.println("basic Program");
         
@@ -18,11 +21,22 @@ public class App
         for(int i = 0; i <= n; i++) {
         	System.out.print(i + " ");
         }
+        System.out.println();
         
         String[] arr = {"Gopi","Afrose","Lohit","Ganesh","Avinash","Venkat"};
         for(String j : arr) {
         	System.out.print(j + " ");
         }
+        System.out.println();
         // The code is been updated by lateral git
+        
+        // The Updated code is from VenkatGit by using Oerations class and creating an object.
+        
+        System.out.println("Addition" + operations.add());
+        System.out.println("Subtraction" + operations.sub());
+        System.out.println("Multiplication" + operations.multiply());
+        System.out.println("Division" + operations.division());
+        
+        
     }
 }
---
+++ src/main/java/com/qualizeal/git/my_firstgit/venkatgit/Oerations.java
@@ -0,0 +1,24 @@
+package com.qualizeal.git.my_firstgit.venkatgit;
+
+public class Oerations {
+	int a = 10;
+	int b = 11;
+	
+	public int add() {
+		int c = a + b;
+		return c;
+	}
+	public int sub() {
+		int c = a-b;
+		return c;
+	}
+	public int multiply() {
+		int c = a*b;
+		return c;
+	}
+	public double division() {
+		double c = a/b;
+		return c;
+	}
+
+}
